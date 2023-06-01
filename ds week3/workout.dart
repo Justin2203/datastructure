190d03fb75a1a44747777d8ba019927a4d81ccb5
Justin Thomas <thomasjustin2203@gmail.com>
2023-06-01 21:43:17 +0530

    week3 latest workouts

--- ds week3/workout.dart
+++ ds week3/workout.dart
@@ -691,3 +691,61 @@
 //     return currentNode!.isEndofword;
 //   }
 // }
+// -----------------------------------------graph dfs--------------------------------------
+
+// abstract class Graph {
+//   Map<String, List<String>> adjacencyList = {};
+
+//   Graph() {
+//     adjacencyList = {};
+//   }
+
+//   void addVertex(String startvertex) {
+//     if (adjacencyList.containsKey(startvertex)) {
+//       adjacencyList[startvertex];
+//     }
+//   }
+
+//   void addEdge(String vertex1, String vertex2) {
+//     adjacencyList[vertex1]?.add(vertex2);
+//     adjacencyList[vertex2]?.add(vertex1);
+//   }
+
+//   void dfs(String vertex) {
+//     Set<String> visited = {};
+
+//     void traverse(String vertex) {
+//       visited.add(vertex);
+//       print('visited vertex:$vertex');
+//     }
+
+//     for (var adjacencyvertex in adjacencyList[vertex] ?? []) {
+//       if (visited.contains(adjacencyvertex)) {
+//         traverse(adjacencyvertex);
+//       }
+//     }
+//   }
+
+//   traverse(startvertex);
+// }
+
+// ---------------------------------dfs graph logic---------------------------
+
+// void dfs(String vertex) {
+//   Set<String> visited = {};
+
+//   void traverse(String vertex) {
+//     visited.add(vertex);
+//     print("visited vertex:$vertex");
+//   }
+
+//   for (var adjacencyvertex in adjacencyList(vertex) ?? []) {
+//     if (visited.contains(adjacencyvertex)) {
+//       treverse(adjacencyvertex);
+//     }
+//   }
+// }
+// traverse(vertex);
+
+
+
